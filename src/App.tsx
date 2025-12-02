c9126f1ffa414526ac1837095b01f3ed4541cd07
Brajendra Rai <brajendra.rai.sp@dlh.de>
2025-12-02 12:37:42 +0530

    desc added

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
     <>
       <div className="card">
         <h3>Welcome to Jio-Hope</h3>
+        <p>This is a starter template for your Jio-Hope project.</p>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
